f8b577e06e18e4d4f123f80a74ef21454b0e1d4f
vahid shahbazi <vahidsh1@gmail.com>
2022-12-29 04:32:05 +0330

    using profiler as an example

---
+++ profiler/src/main/java/com/digipay/profile/ProfilerApplication.java
@@ -0,0 +1,13 @@
+package com.digipay.profile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfilerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfilerApplication.class, args);
+	}
+
+}
